31609038e4eabfb7bc9fbf1909bdb5c9572ffa14
Azamjon99 <oktamovazam1@gmail.com>
2022-08-18 16:42:13 +0500

    book model created

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,11 +15,32 @@ class Book extends Model
     {
         return '/books/' . $this->id;
     }
-    public function setAuthorIdAttribute($author)
+    public function setAuthorIdAttribute(int $author)
     {
         $author = Author::firstOrCreate(['name'=>$author]);
-     
         $this->attributes['author_id'] =  $author->id;
+    }
 
+    public function checkout(User $user)
+    {
+        $this->reservations()->create([
+            'user_id' =>$user->id,
+            'check_out_at' =>now(),
+        ]);
+    }
+    public function checkin(User $user)
+    {
+       $reservation= $this->reservations()->where('user_id', $user->id)->whereNotNull('check_out_at')->whereNull('check_in_at')->first();
+       if(is_null($reservation)){
+        throw new Exception();
+
+       }
+       $reservation->update(['check_in_at'=>now()]);
+
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
